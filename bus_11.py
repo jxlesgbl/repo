5a267a0ffb3bd4990eab8cf5aec3842966bba28d
= <=>
2025-11-27 17:40:19 +0100

    init

---
+++ bus_11.py
@@ -0,0 +1,22 @@
+from flask import Flask, jsonify
+import pandas as pd
+
+URL = "https://data.montpellier3m.fr/sites/default/files/ressources/TAM_MMM_TpsReel.csv"
+
+app = Flask(__name__)
+
+@app.get("/eta")
+def eta():
+    df = pd.read_csv(URL, sep=";")
+    df['delay_min'] = (df['delay_sec'] / 60).round()
+    f = df[
+        (df["route_short_name"] == "11")
+        & (df["direction_id"] == 1)
+        & (df["stop_name"] == "SAPORTA")
+    ].sort_values("delay_min", ascending=True)
+
+    val = f.iloc[0]["delay_min"]
+    return jsonify({"eta": val})
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5500)
